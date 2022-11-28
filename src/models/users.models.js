7ed1458309b80e8cc86cb9b48953c64a81a685c0
Erickbut <erickbutron1@gmail.com>
2022-11-28 16:11:21 -0400

    first commit

--- src/models/users.models.js
+++ src/models/users.models.js
@@ -1,4 +1,44 @@
+/*{
+    id: 1,
+    first_name: 'string',
+    last_name: 'string',
+    email: 'string',
+    password: 'string',
+    birthday: 'YYYY/MM/DD'
+}*/
 
+const { DataTypes } = require('sequelize')
 
+const db = require('../utils/database')
 
+const Users = db.define('users', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  first_name: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  last_name: {
+    type: DataTypes.TEXT,
+    defaultValue: 'Anonymous'
+  },
+  email: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  password: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  birthday: {
+    type: DataTypes.DATE,
+    defaultValue: 'None'
+  }
+}, {
+  timestamps: false //? No agrega las columnas created_at y updated_at
+})
 
+module.exports = Users
--- src/utils/database.js
+++ src/utils/database.js
@@ -1,2 +1,13 @@
+const { Sequelize } = require('sequelize');
+
+const db = new Sequelize({
+  dialect: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: '1498',
+  database: 'CRUD'
+})
 
 
+module.exports = db
